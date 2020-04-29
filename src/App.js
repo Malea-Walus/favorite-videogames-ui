5c361ea8447b2f881f4cdc1d004ef22ac34d3ffb
Malea Walus <millywalus@gmail.com>
2020-04-28 19:54:48 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+export default class App extends React.Component {
+  state = {
+    videoGameList: [], 
+    error: ''
+  }
 
-function App() {
-  const [videoGameList, setVideoGamelist] = useState([])
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/favoriteVideogames`,{
-        method:'GET',
-        mode: 'cors',
-        headers: {
-          'accept': 'application/json'
-        }
-      })
-      const data = await response.json()
-      setVideoGamelist(data)
-    }
-    fetchData()
-  }, []);
+  save = async (videoGame) => {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/favoriteVideogames`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(videoGame)
+    })
 
-  return (
-    <div className="App">
-      <ul>
-        {videoGameList.map(game => {
-          return (<li key={game._id}>{game.name}</li>) 
-        })}
-      </ul>
-    </div>
-  );
+    const succesful = response.status === 201
+
+  }
+
+  fetchData = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/favoriteVideogames`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'accept': 'application/json'
+      }
+    })
+    const data = await response.json()
+    this.setState({videoGameList:data}) 
+
+    
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  renderError = () => {
+    return this.state.error
+      ? (<div>{this.state.error.message}</div>)
+      : (<Fragment />)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.videoGameList.map(game => {
+            return (<li key={game._id}>{game.name}</li>)
+          })}
+        </ul>
+        <button onClick={this.save}>save</button>
+      </div>
+
+    )
+  }
 }
-
-export default App;
